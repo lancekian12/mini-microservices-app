f8d6837f740558ee2556179fc0f3150247d2f5d3
Lance Kian Flores <lancekian12@gmail.com>
2026-04-19 10:54:50 +0800

    feat: create comment service

--- posts/index.js
+++ posts/index.js
@@ -5,11 +5,18 @@ const { randomBytes } = require("crypto");
 const app = express();
 const posts = {};
 
-app.get("/posts", (req, res) => {});
+app.use(bodyParser.json());
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
 app.post("/posts", (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
+  if (!title) {
+    return res.status(400).send({ error: "Title is required" });
+  }
   if (title) {
     posts[id] = { id, title };
 
