4ce475fa7b52927ffe0230f33bf20a50b9a83485
Lance Kian Flores <lancekian12@gmail.com>
2026-04-21 11:26:24 +0800

    feat: basic implementation of event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const axios = require('axios')
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/events").catch((err) => console.log(err));
+  axios.post("http://localhost:4001/events").catch((err) => console.log(err));
+  axios.post("http://localhost:4002/events").catch((err) => console.log(err));
+
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+    console.log("Listening in 4005")
+})
