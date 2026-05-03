92ef046e55278331be80bd3adeb30a6d3714cf4a
Lance Kian Flores <lancekian12@gmail.com>
2026-05-04 00:03:14 +0800

    create query service for endpoint /events

--- query/index.js
+++ query/index.js
@@ -1,17 +1,30 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
+const post = {};
 
-app.use(bodyParser())
-app.use(cors())
+const app = express();
 
+app.use(bodyParser());
+app.use(cors());
 
-app.get('/posts', (req, res) => { })
+app.get("/posts", (req, res) => {});
 
-app.post('events', (req, res) => { })
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  if (type === "PostCreated") {
+    const { id, title } = data;
 
-app.listen('4002', () => {
-    console.log('Listening on 4002')
-})
+    post[id] = { id, title, comments: [] };
+  }
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+    const post = post[postId];
+    post.comments.push({ id, content });
+  }
+});
+
+app.listen("4002", () => {
+  console.log("Listening on 4002");
+});
