4b9d5ac74f55171a54cfa540bd1501b95012233c
Lance Kian Flores <lancekian12@gmail.com>
2026-05-05 02:19:41 +0800

    feat: comment done

--- comments/index.js
+++ comments/index.js
@@ -18,11 +18,10 @@ app.get("/posts/:id/comments", (req, res) => {
 app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
-  const
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content, status: 'pending'});
+  comments.push({ id: commentId, content, status: "pending" });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -32,7 +31,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content: content,
       postId: req.params.id,
-      status: 'pending '
+      status: "pending ",
     },
   });
 
@@ -41,21 +40,26 @@ app.post("/posts/:id/comments", async (req, res) => {
 
 app.post("/events", async (req, res) => {
   console.log("Received Event", req.body.type);
-  const { type, data } = req.body
+  const { type, data } = req.body;
   if (type === "CommentModerated") {
-    const { postId, id, status, content} = data;
-    const comments = commentsByPostId[postId]
-    const comment = comments.find(comment => {
-      return comment.id === id
-    })
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
     comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    });
   }
-  await axios.post('http://localhost:4005/events', {
-    type: "CommentUpdated",
-    data: {
-      id, postId, status, content
-    }
-  })
+
   res.send({});
 });
 
--- moderation/index.js
+++ moderation/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("bodyParser");
+const bodyParser = require("body-parser");
 const axios = require('axios')
 
 const app = express();
 
 app.use(bodyParser());
-app.use(cors());
 
 app.post("/events", async (req, res) => {
   const { type, data } = req.body;
