bede4762169aad164993d6b84cd2552a225f096c
Lance Kian Flores <lancekian12@gmail.com>
2026-05-07 01:43:47 +0800

    feat: rendering comment by status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 const CommentList = ({ comments }) => {
-  const renderedComments = comments.map((data) => {
-    return <li id={data.id}>{data.content}</li>;
+  const renderedComments = comments.map((comment) => {
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+    return <li key={comment.id}>{content}</li>;
   });
   return <ul>{renderedComments}</ul>;
 };
--- moderation/index.js
+++ moderation/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const axios = require('axios')
+const axios = require("axios");
 
 const app = express();
 
@@ -8,21 +8,24 @@ app.use(bodyParser());
 
 app.post("/events", async (req, res) => {
   const { type, data } = req.body;
-  if (type === 'CommentCreated') {
-    const status = data.content.includes('orage') ? 'rejected' : 'approved';
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+    await axios
+      .post("http://localhost:4005/events", {
+        type: "CommentModerated",
+        data: {
+          id: data.id,
+          postId: data.postId,
+          status,
+          content: data.content,
+        },
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }
-  await axios.post('http://localhost:4005/events', {
-    type: 'CommentModerated',
-    data: {
-      id: data.id,
-      postId: data.postId,
-      status,
-      content: data.content
-    }
-  }).catch((err) => {
-    console.log(err.message)
-  })
-  res.send({})
+
+  res.send({});
 });
 
 app.listen("4003", () => {
